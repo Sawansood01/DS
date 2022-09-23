d4805a619b40a16e89c602cb366da947c6b1cade
Sawan Sood <65075847+Sawansood01@users.noreply.github.com>
2022-09-23 08:32:28 +0530

    Time: 1 ms (88.14%), Space: 63.9 MB (81.01%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -2,9 +2,9 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
         k = k % n;
-        reverse(nums , 0 , n-1);
-          reverse(nums , 0 , k-1);
-          reverse(nums , k , n-1);
+        reverse(nums , 0 , n-1); // this will reverse full array first  5,4,3,2,1
+          reverse(nums , 0 , k-1);  // reverse 0 to k-1 ,k = 3  3,4,5,2,1
+         reverse(nums , k , n-1);  // reverse k to n-1 3,4,5,1,2
     }
         public void reverse(int[] nums , int start , int end)
         {
